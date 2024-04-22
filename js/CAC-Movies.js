e458b468b0fa37076b68e2f23cfea89fe8b50e67
alegonzalezM <68816355+alegonzalezM@users.noreply.github.com>
2024-04-22 18:30:01 -0300

    Add files via upload

---
+++ js/CAC-Movies.js
@@ -0,0 +1,40 @@
+AOS.init({
+    offset: 120,
+    delay: 100,
+    duration: 1000,
+    easing: 'ease',
+
+});
+
+// $(window).scroll(function() {
+//   var scrollPos = $(window).scrollTop();
+//   var triggerPos = 900; // 
+  
+//   if (scrollPos > triggerPos) {
+//     $(".section__find").slideDown(1000);
+//   } 
+//   else {
+//     $(".section__find").slideDown(5000);
+//   }
+// });
+
+
+// $(document).ready(function(){    
+// 	 $("#imagen").click(function(){
+// 	 $(this).animate(
+// 	 {marginLeft: "+=50px"}, 60); });});
+// ----------------------------------------------------
+//     $(document).ready(function(){
+// 	    $("#imagen").mouseover(function(){
+// 	    $(this).animate( { opacity:1 }, 250 ); });
+// 	$("#imagen").mouseout(function(){
+// 	    $(this).animate({ opacity:0.5} , 250 ); });});
+// //-----------------------------------------------------------------
+//         $(document).ready(function(){
+// 			$("#titulo").hide();
+// 			$("#imagen").width(60).height(40).click(function(){   //la imagen inicia pequeña
+// 			$(this).animate({ 
+// 			marginLeft: "10px", width: "200px", height: "200px"}, 1500, function(){
+//                 $("#titulo").fadeIn(1000).delay(1000).fadeOut(1000);  
+//             });});});  //1500 es el tiempo
+//-----------------------------------------------------

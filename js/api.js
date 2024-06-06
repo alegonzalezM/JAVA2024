92a2659364f4a05655e8e75dc2b15f638b9c4803
alegonzalezM <68816355+alegonzalezM@users.noreply.github.com>
2024-06-06 09:49:12 -0300

    Update api.js

--- js/api.js
+++ js/api.js
@@ -1,4 +1,6 @@
 
+/*---------Conecta con API------*/
+
 let todasLasPeliculas = [];
 let paginaAPI = 1;
 let paginaActual = 1;
@@ -19,13 +21,16 @@ async function traer() {
         const response = await fetch(urlBase);
         const data = await response.json();
 
-        // Asumiendo que data es un array de películas
+        // un array de películas
         todasLasPeliculas = data;
         mostrarPeliculas();
     } catch (error) {
         console.error('Error:', error);
     }
 }
+
+
+/*---------Arma listado y muestra en pantalla--------*/
 
 function mostrarPeliculas() {
     const inicio = (paginaActual - 1) * peliculasPorPagina;
@@ -35,9 +40,14 @@ function mostrarPeliculas() {
     let contenido = "";
     peliculasParaMostrar.forEach(pelicula => {
         let titulo = `${pelicula.Title}`.substring(0, 25);
+ 
         contenido += `
-            <a href="details.html">
-                <div class="picture-container-api">
+        <a href="details.html?title=${encodeURIComponent(pelicula.Title)}&poster=${encodeURIComponent(pelicula.Poster)}
+        &released=${encodeURIComponent(pelicula.Released)}&genre=${encodeURIComponent(pelicula.Genre)}&runtime=
+        ${encodeURIComponent(pelicula.Runtime)}&plot=${encodeURIComponent(pelicula.Plot)}&director=
+        ${encodeURIComponent(pelicula.Director)}&actors=${encodeURIComponent(pelicula.Actors)}&writer=
+        ${encodeURIComponent(pelicula.Writer)}&trailer=${encodeURIComponent(pelicula.Trailer)}">
+        <div class="picture-container-api">
                     <picture class="movie__pic rounded mx-auto">
                         <img src="${pelicula.Poster}" alt="${pelicula.Title}">
                     </picture>
@@ -48,39 +58,87 @@ function mostrarPeliculas() {
     });
 
     document.querySelector(".container__movies").innerHTML = contenido;
-
-    // Desactivar/activar botones de navegación
-    if (fin >= todasLasPeliculas.length) {
-        nextBtn.classList.add("disabled");
-    } else {
-        nextBtn.classList.remove("disabled");
-    }
-
-    if (paginaActual === 1) {
-        prevBtn.classList.add("disabled");
-    } else {
-        prevBtn.classList.remove("disabled");
-    }
 }
 
-nextBtn.addEventListener('click', function() {
-    cambiarPagina(1);
+/*****Envia info por parametros a details.html************************/
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('Script loaded')
+    const containerDetails = document.querySelector('.container__details');
+
+    function getQueryParams() {
+        const params = new URLSearchParams(window.location.search);
+        return {
+            title: params.get('title'),
+            poster: params.get('poster'),
+            released: decodeURIComponent(params.get('released')),
+            genre: params.get('genre'),
+            runtime: params.get('runtime'),
+            plot: params.get('plot'),
+            director: params.get('director'),
+            actors: params.get('actors'),
+            writer: params.get('writer'),
+            trailer: params.get('trailer')
+
+        };
+    }
+
+    const peliculaSeleccionada = getQueryParams();
+    console.log(peliculaSeleccionada)
+
+    if (peliculaSeleccionada.title && peliculaSeleccionada.poster) {
+       
+
+        // containerDetails.style.backgroundImage = `url(${peliculaSeleccionada.poster})`;
+        containerDetails.style.background = `linear-gradient(rgba(80, 75, 75, 0.792), rgba(251, 246, 246, 0.8)), url(${peliculaSeleccionada.poster})`;
+        containerDetails.style.backgroundSize = 'cover';
+        containerDetails.style.backgroundPosition = 'center';
+
+
+
+        const contenido_details = ` 
+                <div class="container__movie">
+                <div class="image__movie"><img src="${peliculaSeleccionada.poster}" alt="${peliculaSeleccionada.title}"></div>
+                <div class="detalle__movie">
+                    <h1>${peliculaSeleccionada.title}</h1>
+                    <p>${peliculaSeleccionada.released} • ${peliculaSeleccionada.genre} • ${peliculaSeleccionada.runtime}</p>
+                    <h2>Descripción:</h2>
+                    <p>${peliculaSeleccionada.plot}</p>
+                </div>
+                <div id="director__movie_1">
+                    <h3>${peliculaSeleccionada.director}</h3>
+                    <p>Director</p>
+                </div>
+                <div id="director__movie_2">
+                    <h3>${peliculaSeleccionada.actors}</h3>
+                    <p>Actores</p>
+                </div>
+                <div id="director__movie_3">
+                    <h3>${peliculaSeleccionada.writer}</h3>
+                    <p>Autor</p>
+                </div>
+            </div>
+            <section class="container container__show_movie">
+                <div>
+                    <h2>Ver trailer</h2>
+                    <iframe width="560" height="315" src="${peliculaSeleccionada.trailer}"  allowfullscreen></iframe>
+                </div>
+                <div class="redes_soc">
+                    <ul>
+                        <li><a href="https://www.facebook.com/supermariomovie" target="_blank"><i class="fab fa-facebook"></i></a></li>
+                        <li><a href="https://twitter.com/supermariomovie" target="_blank"><i class="fab fa-twitter"></i></a></li>
+                        <li><a href="https://instagram.com/supermariomovie/" target="_blank"><i class="fab fa-instagram"></i></a></li>
+                        <li><a href="https://www.thesupermariobros.movie/" target="_blank"><i class="fas fa-link"></i></a></li>
+                    </ul>
+                </div>
+            </section>
+        `;
+
+        containerDetails.innerHTML = contenido_details;
+    } else {
+        containerDetails.innerHTML = '<p>No se ha seleccionado ninguna película.</p>';
+    }
 });
 
-prevBtn.addEventListener('click', function() {
-    cambiarPagina(-1);
-});
 
-function cambiarPagina(cambio) {
-    paginaActual += cambio;
-    mostrarPeliculas();
-}
-
-function foco() {
-    var movieElement = document.getElementById('tendencias');
-    if (movieElement) {
-        movieElement.scrollIntoView({ behavior: 'smooth' });
-    }
-}
 
 
